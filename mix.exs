2cdd657757d82a5cc688b6a1d92d99ba1c839221
Milad Rastian <eslashmili@gmail.com>
2018-08-23 15:31:36 +0200

    Update dialyzer

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule HMCrypto.MixProject do
   defp deps do
     [
       {:credo, "~> 0.8.10", only: [:dev, :test]},
-      {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.3", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.18.1", only: :dev},
       {:earmark, "~> 1.2", only: :dev}
     ]
