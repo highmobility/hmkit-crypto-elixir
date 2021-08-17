8933f0d0e58e12655d9bd11f837b4b6e2564f853
Milad Rastian <milad@high-mobility.com>
2021-08-17 10:42:42 +0200

    eanble codecov coverage

--- mix.exs
+++ mix.exs
@@ -7,6 +7,10 @@ defmodule HmCrypto.MixProject do
       version: "2.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      preferred_cli_env: [
+        "coveralls.json": :test
+      ],
+      test_coverage: [tool: ExCoveralls],
       deps: deps()
     ]
   end
@@ -25,7 +29,8 @@ defmodule HmCrypto.MixProject do
       {:credo, "~> 1.5", only: [:dev, :test]},
       {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.18.1", only: :dev},
-      {:earmark, "~> 1.2", only: :dev}
+      {:earmark, "~> 1.2", only: :dev},
+      {:excoveralls, "~> 0.14.2", only: [:test], runtime: false}
     ]
   end
 end
